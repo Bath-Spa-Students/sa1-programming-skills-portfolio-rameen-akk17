13c99496d7340ac9fba33fb843a03f9c243c16d1
rameen-akk17 <akram_rm@icloud.com>
2025-03-01 12:18:47 +0400

    coding is cool

---
+++ Exercise1.py
@@ -0,0 +1,7 @@
+#assigning values to variables 
+word1= "Coding"
+word2= "is"
+word3= "cool"
+
+##Concatenating wothe variables  and using double qoutations for the proper spacing for the print statements 
+print(word1 + " " + word2 + " " + word3)

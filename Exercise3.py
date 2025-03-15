03579f1f2c020efb850294cf47db575e21691254
rameen-akk17 <akram_rm@icloud.com>
2025-03-15 13:02:15 +0400

    Biography

---
+++ Exercise3.py
@@ -0,0 +1,9 @@
+#storing information as keyvalue pairs in a dictionary
+info = {
+    "name":"rameeen akram",        #string data type
+    "hometown":"lahore",    #string data type
+    "age":19                 # Integer data type
+}
+
+#prints values on separate lines using a single print statement
+print(f"Name: {info['name']}\nHometown: {info['hometown']}\nAge: {info['age']}")
